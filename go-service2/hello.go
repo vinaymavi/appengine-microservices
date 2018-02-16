756c2ab7740f7257ae366cbc1055510f3abf8f43
VinayMavi <vinaymavi@gmail.com>
2018-02-16 18:06:25 +0530

    Go-lang micro service added.

---
+++ go-service2/hello.go
@@ -0,0 +1,14 @@
+package go_service2
+
+import (
+    "fmt"
+    "net/http"
+)
+
+func init() {
+    http.HandleFunc("/", handler)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprint(w, "Hello, world! Go......")
+}
